065010253ce94b29430696d8d6194e8d58f5500e
gauthier <gauthier.mure@gmail.com>
2022-07-22 09:52:35 +0200

    final update

--- app/src/androidTest/java/com/example/maru/ApplicationInstrumentedTest.java
+++ app/src/androidTest/java/com/example/maru/ApplicationInstrumentedTest.java
@@ -6,6 +6,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
@@ -13,6 +14,12 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 import android.view.View;
 
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -37,6 +44,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @RunWith(AndroidJUnit4.class)
 public class ApplicationInstrumentedTest {
@@ -44,12 +52,49 @@ public class ApplicationInstrumentedTest {
     private int ITEMS_COUNT;
 
     private ApiServiceMeeting apiServiceMeeting;
-    private ApiServiceMeetingRoom apiServiceMeetingRoom;
+
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
 
     @Before
     public void setup(){
         apiServiceMeeting = DI.getMeetingApiService();
-        apiServiceMeetingRoom = DI.getNewInstanceMeetingRoomApiService();
         ITEMS_COUNT = apiServiceMeeting.getMeetings().size();
     }
 
@@ -67,20 +112,41 @@ public class ApplicationInstrumentedTest {
         onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(ITEMS_COUNT-=1)));
     }
 
-    //@Test
-    /*public void MeetingList_FilterByRoomA_ShouldDisplayOneChild(){
-        ActivityScenario<HomePageActivity> scenario = mActivityRule.getScenario();
+    @Test
+    public void MeetingList_FilterByRoomA_ShouldDisplayOneChild(){
+        ActivityScenario<HomePageActivity> scenario = ActivityScenario.launch(HomePageActivity.class);
+        scenario.recreate();
+
+        onView(withId(R.id.ibtnFilter)).perform(click());
+        onView(withText("annuler")).perform(click());
+
         onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(ITEMS_COUNT)));
-        try {
-            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        }catch (Exception e){}
-        //onView(withId(R.id.ibtnFilter)).perform(click());
+        onView(withId(R.id.ibtnFilter)).perform(click());
         onView(withText("lieu")).perform(click());
 
+        onView(withId(R.id.filterListRv)).perform(RecyclerViewActions.actionOnItemAtPosition(1,click()));
+        onView(withId(R.id.filterSelectBtn)).perform(click());
 
+        onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(1)));
+    }
 
-        //onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(1)));
-    }*/
+    @Test
+    public void MeetingList_FilterByDate_ShouldDisplayOneChild(){
+        ActivityScenario<HomePageActivity> scenario = ActivityScenario.launch(HomePageActivity.class);
+        scenario.recreate();
+
+        onView(withId(R.id.ibtnFilter)).perform(click());
+        onView(withText("annuler")).perform(click());
+
+        onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(ITEMS_COUNT)));
+        onView(withId(R.id.ibtnFilter)).perform(click());
+        onView(withText("date")).perform(click());
+
+        onView(withId(R.id.datefilterPicker)).perform(PickerActions.setDate(2022,5,30));
+        onView(withId(R.id.selectDateFilterBtn)).perform(click());
+
+        onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(1)));
+    }
 
     @Test
     public void MeetingList_ItemClic_ShouldDisplayMeetingInfo(){
@@ -95,12 +161,10 @@ public class ApplicationInstrumentedTest {
     @Test
     public void MeetingList_addMeeting(){
         ActivityScenario<HomePageActivity> scenario = ActivityScenario.launch(HomePageActivity.class);
+        scenario.recreate();
 
         onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(ITEMS_COUNT)));
-
         onView(withId(R.id.IBtnaddMeeting)).perform(click());
-        onView(withId(R.id.createMeetingFragment)).check(matches(isDisplayed()));
-        onView(withId(R.id.createMeetingBtn)).check(matches(isDisplayed()));
         onView(withId(R.id.createMeetingBtn)).perform(click());
 
         onView(withId(R.id.rvListMeeting)).check(matches(hasChildCount(ITEMS_COUNT+=1)));
